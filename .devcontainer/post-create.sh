653d0056bf5044ecd08ae4f12e8f19ffccc9f25e
Hiroto / hiroto furugen <hiro_genfuru0119@icloud.com>
2025-06-06 17:53:14 +0000

    コンテナ設定

---
+++ .devcontainer/post-create.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# This script will be run after the container is created
+# You can add any custom setup here
+
+# Example: echo "Welcome to your Codespace!"
+echo "Welcome to your Business Management AI Platform Codespace!"
